925b4055ac490bc52d063c80bd5426965d15d957
SirleneTeixeira <sirleneteixeiraadv@gmail.com>
2025-05-16 01:51:56 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Direito Descomplicado!</h1>
+      <p>Esse é o início do seu SaaS jurídico trabalhista.</p>
+    </div>
+  );
+}
+
+export default App;
